909170c3d0180d2790e76e5522477c183efa55e7
Ricardo <ricardokae.rj@gmail.com>
2022-06-03 00:52:23 -0300

    + ServidorCentral, RichTextOnTerminal, Usuario

--- ServidorCentral.py
+++ ServidorCentral.py
@@ -1,63 +1,156 @@
-import socket
-import json
-import select
-import threading, sys
+#-*- encoding: utf-8
+
+import json, socket, threading, select, sys
+
+''' 
+Servidor Central do chat distribuído
+'''
 
 class ServidorCentral:
-	def __init__(self, HOST, PORTA, nConexoes):
-	    self.host = HOST
-		self.porta = PORTA
-		self.nConexoes = nConexoes
-		self.entradas = [sys.stdin]
-		self.conexoes = {}
+    # Parâmetros da classe
+    usuariosOnline = {}       # HashMap (dicionário) que armazena info dos usuários online
+    lock = threading.Lock()   # Lock para evitar condições de corrida no dicionário acima
+    
+    # Construtor da classe #
+    # // Entrada: Um HOST e PORTA para aguardar conexões e o número de Conexões possíveis
+    def __init__(self, HOST, PORTA, nConexoes):
+        self.HOST = HOST
+        self.PORTA = PORTA
+        self.nConexoes = nConexoes
+        
+        # Inclui (stdin - Entrada padrão) na lista de entradas a ser selecionada enquanto
+        self.entradas = [sys.stdin] # aguarda o processamento I/O (entrada e saída dos sockets)
+        self.aguardarConexoes()
+        
+    # Método que inicia o socket para aguardar conexões #
+    def aguardarConexoes(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock.bind((self.HOST, self.PORTA))
+        print("Servidor Central aguardando conexões...")
+        self.sock.listen(self.nConexoes)
+        self.sock.setblocking(False)
+        self.entradas.append(self.sock) # 
 
-	def iniciaServidor(self):
-		#cria o socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Internet(IPv4 + TCP)
-		#vincula a localizacao do servidor
-		sock.bind((self.host, self.porta))
-		#coloca-se em modo de espera por conexoes
-		sock.listen(self.nConexoes)
-		return sock
+    # Método que aceita as conexões #
+    def aceitarConexoes(self):
+        cliSock, endereco = self.sock.accept()
+        print("Conectado com: ", endereco)
+        return (cliSock, endereco)
+        
+    # Método que exibe os comandos do servidor #
+    def exibirComandos(self):
+        print("Comandos aceitos: \n \
+            1. info: Informações sobre o servidor \n \
+            2. get_lista: Lista usuários online \n \
+            3. comandos: Exibir comandos \n \
+            4. exit: Encerra o servidor. (!) Se houver usuários online, são todos deslogados")
+        
+    # Método (start) principal do Servidor ( inicia o processamento dele ) #
+    # // Chama o aceitarConexoes (acima), que designa threads para atender cada requisição de cada conexão feita
+    # // E lida (handler) com os comandos da entrada padrão (stdin)
+    def start(self): 
+        print("Digite 'comandos' para saber os comandos do Servidor")
+        while True:
+            leitura, escrita, excecao = select.select(self.entradas, [], [])
+            for entrada in leitura:
+                if entrada == self.sock:
+                    clisock, endereco = self.aceitarConexoes()
+                    print("Aguardando requisições de " + str(endereco))
+                    usuario_onthread = threading.Thread(target = self.atenderRequisicoes, args = (clisock, endereco))
+                    usuario_onthread.start()
+                elif entrada == sys.stdin:
+                    comandoEntrada = input()
+                    if comandoEntrada == "info":
+                        print("Host: " + str(self.HOST) + '\n' + "Porta: " + str(self.PORTA) + '\n' + "Nº conexões possíveis: " + str(self.nConexoes))
+                    elif comandoEntrada == "get_lista":
+                        if not ServidorCentral.usuariosOnline: print("Nenhum usuário online")
+                        for usuario in ServidorCentral.usuariosOnline:
+                            print(usuario)
+                    elif comandoEntrada == "comandos":
+                        self.exibirComandos()
+                    elif comandoEntrada == "exit":
+                        # Precisa implementar o deslogamento de todos os usuários online, que provavelmente tem haver com o encerramento de threads (join)
+                        # Ele desliga o servidor, mas as threads ativas continuam rodando. Tem que resolver isso
+                        self.sock.close()
+                        exit(1)
 
-	def aceitaConexao(self, sock):
-		#estabelece conexao com o proximo cliente
-		cliente_sock, endereco = sock.accept()
-		return cliente_sock, endereco
-		
-	def atendeRequisicoes(self, cliSock, endereco):
-		while True:
-			data = cliSock.recv(1024)
-			if not data:
-				print(str(endereco)+'-> encerrou')
-				lock.acquire()
-				del conexoes[cliSock]
-				lock.release()
-				cliSock.close()
-				return
-			respostaCliente = bytes(data, 'utf-8')
-			cliSock.send(respostaCliente)
-
-servidor = ServidorCentral('', 5001, 2)
-sock = servidor.iniciaServidor()
-lock = threading.Lock()
-while True:
-	leitura, escrita, excecao = select.select(servidor.entradas, [], [])
-	#tratar todas as entradas prontas
-	for pronto in leitura:
-		if pronto == sock: #pedido novo de conexao
-			cliente_sock, endereco = servidor.aceitaConexao(sock)
-			print('Conectado com: ' + str(endereco))
-			#cria novo processo para atender o cliente
-			cliente = threading.Thread(target = servidor.atendeRequisicoes, args = (cliente_sock, endereco))
-			cliente.start()
-			clientes.append(cliente) #armazena a referencia da thread para usar com join()
-		elif pronto == sys.stdin: #entrada padrao
-			cmd = input()
-			if cmd == 'fim': #solicitacao de finalizacao do servidor
-				for c in clientes: #aguarda todos os processos terminarem
-					c.join()
-				sock.close()
-				sys.exit()
-			elif cmd == 'hist':
-                		print(str(servidor.conexoes.values()))
+    # Método executado pelas threads para atender (handler) as requisições de cada cliente #
+    # // Entrada: O socket do cliente, no qual as threads executam o receive para receber dados simultaneamente e o endereco deles
+    def atenderRequisicoes(self, clisock, endereco):
+        while True:
+            bytesRecebidos = clisock.recv(1024)
+            dados = str(bytesRecebidos, "utf-8")
+            dadosJSON = json.loads(dados)
+            operacao_usuario = dadosJSON["operacao"]
+            if operacao_usuario == "login":
+                username_usuario = dadosJSON["username"]
+                status, mensagem = self.registrarUsuarioON(username_usuario, endereco, dadosJSON["porta"])
+            elif operacao_usuario == "logoff":
+                username_usuario = dadosJSON["username"]
+                status, mensagem = self.deslogarUsuario(username_usuario)
+            elif operacao_usuario == "get_lista":
+                status, mensagem = self.getUsuariosOnline()
+            else: # Se o cliente tiver tratamento de erro quanto as requisições, essa condição nunca roda
+                print("Comando inválido recebido de ", clisock)
+            
+            self.enviarResposta(clisock, operacao_usuario, status, mensagem) # Envia as respostas de cada requisição
+           
+    # Abaixo os Event Handlers para cada operação requisitada #
+    
+    # Método que registra um usuário online devido ao evento (requisição) de login #
+    # // Entrada: Username, endereco e porta do usuario
+    # // Saída: Tupla contendo o (status, mensagem) da operacao (evento) requisitado
+    def registrarUsuarioON(self, username, endereco, porta):
+        if not ServidorCentral.usuariosOnline.get(username):
+            # Condição de corrida
+            ServidorCentral.lock.acquire()
+            ServidorCentral.usuariosOnline[username] = {
+                "Endereco": endereco[0], "Porta": str(porta)
+                }
+            print("@" + username + " conectado")
+            ServidorCentral.lock.release()
+            # Fim da condição de corrida
+            return (200, "Login com sucesso")
+        return(400, "Username em Uso")
+                    
+    # Método que desregistra um usuário online devido ao evento (requisição) de logoff #
+    # // Entrada: Username a ser deslogado
+    # // Saída: Tupla contendo o (status, mensagem) da operação 
+    def deslogarUsuario(self, username):
+        if ServidorCentral.usuariosOnline.get(username):
+            # Condição de corrida
+            ServidorCentral.lock.acquire()
+            del ServidorCentral.usuariosOnline[username]
+            print("@" + username + " desconectado")
+            ServidorCentral.lock.release()
+            # Fim da condição de corrida
+            return (200, "Logoff com sucesso")
+        return (400, "Erro no Logoff")
+    
+    # Método que #
+    def getUsuariosOnline(self):
+        clientes = ServidorCentral.usuariosOnline # vai sempre funcionar
+        return (200, clientes)
+    
+    # Método #
+    def enviarResposta(self, clisock, operacao, status, mensagem):
+        resposta_usuario = {
+            "operacao": operacao, 
+            "status": status,
+        }
+        if operacao != "get_lista": resposta_usuario["mensagem"] = mensagem
+        else: resposta_usuario["clientes"] = mensagem
+        respostaJSON = json.dumps(resposta_usuario)
+        respostaBytes = bytes(respostaJSON, "utf-8")
+        clisock.sendall(respostaBytes)
+    
+# Função principal do Servidor #
+def main():
+    HOSTSC = ''
+    PORTASC = 9131
+    nConexoes = 3
+    servidor = ServidorCentral(HOSTSC, PORTASC, nConexoes)
+    servidor.start()
+    
+if __name__ == "__main__":
+    main()
